115049028d13f904494e346a55ec1e9131e9ecb4
quinnhyx <123648507+quinnhyx@users.noreply.github.com>
2023-04-27 16:25:36 -0400

    Add files via upload

--- Detective.java
+++ Detective.java
@@ -14,7 +14,7 @@ public class Detective {
         this.applianceloc = null;
     
     }
-    public void goTo(Room room){
+    public void walkTo(Room room){
         this.location=room;
         System.out.println("You are in "+room.name+" now");
         this.location.printProfile();
@@ -25,6 +25,9 @@ public class Detective {
         this.location.printItem();
     }
     public void grab(Artifact item){
+        if (this.backpack.contains(item)){
+            throw new RuntimeException("it's already in your bag!");
+        }
         this.backpack.add(item);
         System.out.println("You just grabed "+item.name);
     }
@@ -33,7 +36,7 @@ public class Detective {
             this.backpack.remove(item);
         System.out.println("You just dropped "+item.name);
         }else{
-            throw new RuntimeException(item+" is not in your backpack!");
+            throw new RuntimeException(item.name+" is not in your backpack!");
         }
     }
     public void examine(Artifact item){
@@ -41,27 +44,33 @@ public class Detective {
         item.getDescription();
         steps++;
     }
-
-    public void walkTo(Appliance appliance){
+    public void goTo(Appliance appliance){
         System.out.print("You are at "+appliance.name+". There are");
-        
+        this.applianceloc = appliance;
         if (applianceloc==null){
             System.out.println("please use walk to(item) method.");
         }
-        this.applianceloc = appliance;
+        
         appliance.printItem();
         
 
     }
+    public void checkBag(){
+        for (int i=0; i<backpack.size();i++){
+            System.out.println(backpack.get(i).name);
+        }
+    }
+    
     public void showOptions() {
         System.out.println("Available options:");
         System.out.println("+ show rooms");
-        System.out.println(" + go to(room)");
-        System.out.println(" + walk to(appliance)");
+        System.out.println(" + walk to(room)");
+        System.out.println(" + go to(appliance)");
         System.out.println(" + look around");
         System.out.println(" + examine(item)");
         System.out.println(" + grab(item)");
         System.out.println(" + drop(item)");
+        System.out.println("+ check bag()");
         System.out.println("+ declare(suspect)");
         System.out.println("+ showReport()");
       }
--- Game.java
+++ Game.java
@@ -16,69 +16,69 @@ public class Game {
         // }
             
         Hashtable<String, Appliance> nurseAppliance = new Hashtable<String, Appliance>();
-        Artifact diary=new Artifact("Diary", "poor wage, not enough to buy food and clothes");
-        Artifact ring=new Artifact("Wedding ring", "xx");
-        Appliance Table1 = new Appliance("Table");
-        Table1.put("Diary",diary);
-        Table1.put("Ring", ring);
-        nurseAppliance.put("Table",Table1);
+        Artifact diary=new Artifact("diary", "poor wage, not enough to buy food and clothes");
+        Artifact ring=new Artifact("wedding ring", "null");
+        Appliance Table1 = new Appliance("table");
+        Table1.put("diary",diary);
+        Table1.put("ring", ring);
+        nurseAppliance.put("table",Table1);
         Room nurseStation = new Room("Nurse Station",nurseAppliance, "Head nurse: bloodType = O; 28 years old; female; 168cm, 48kg; salary= 5000; description: 18:05 received call from dean, asked to meet at 20:00; 18:10 called assistant dean about dean, meet with assistant dean at 19:50 in the hospital.");
 
         Hashtable<String, Appliance> investorOfficeAppliance = new Hashtable<String, Appliance>();
         Artifact shareProspectus=new Artifact("share prospectus", "investor 55%, dean 30%, assistant dean 15%");
-        Artifact photo=new Artifact("Photos", "Photos with ex");
-        Artifact newspaperA=new Artifact("Newspaper A", "investor inherited 1 billion dollars from parents who died in car ");
-        Artifact newspaperB=new Artifact("Newspaper B", "one of the ex died because of dean's surgery failure ");
+        Artifact photo=new Artifact("photos", "Photos with ex");
+        Artifact newspaperA=new Artifact("newspaper A", "investor inherited 1 billion dollars from parents who died in car ");
+        Artifact newspaperB=new Artifact("newspaper B", "one of the ex died because of dean's surgery failure ");
         
-        Appliance tableDrawer = new Appliance("Table Drawer");
-        tableDrawer.put("Share Prospectus",shareProspectus);
-        tableDrawer.put("Photo", photo);
-        tableDrawer.put("NewspaperA", newspaperA);
-        tableDrawer.put("NewspaperB", newspaperB);
-        investorOfficeAppliance.put("Table Drawer",tableDrawer);
+        Appliance tableDrawer = new Appliance("drawer");
+        tableDrawer.put("share prospectus",shareProspectus);
+        tableDrawer.put("photo", photo);
+        tableDrawer.put("newspaperA", newspaperA);
+        tableDrawer.put("newspaperB", newspaperB);
+        investorOfficeAppliance.put("drawer",tableDrawer);
         Artifact email = new Artifact("email with assistant dean","assistant dean wants to replace dean after his brother disappeared, but investor rejects him because investor likes dean more.");
-        Appliance laptop = new Appliance("Laptop");
+        Appliance laptop = new Appliance("laptop");
         laptop.put("email", email);
         Artifact liquorBottle = new Artifact("liquor bottles",null);
         Appliance shelf = new Appliance("Shelf");
-        shelf.put("Liquor Bottle", liquorBottle);
-        investorOfficeAppliance.put("table drawer", tableDrawer);
+        shelf.put("liquor bottle", liquorBottle);
+        investorOfficeAppliance.put("drawer", tableDrawer);
         investorOfficeAppliance.put("laptop", laptop);
         investorOfficeAppliance.put("shelf",shelf);
         Room InvestorOffice = new Room("Investor's Office",investorOfficeAppliance, "Investor: bloodType = O; 32 years old; male; 178cm, 75kg; description: 18:00 received call from dean; arrived at hospital at 20:00.");
 
 
         Hashtable<String, Appliance> wardAppliance = new Hashtable<String, Appliance>();
-        Appliance bed = new Appliance("Bed");
+        Appliance bed = new Appliance("bed");
         Artifact Bed = new Artifact("bed", null);
-        bed.put("Bed",Bed);
-        Appliance drawer = new Appliance("Drawer");
-        Artifact newspaper=new Artifact("newspaper", "he and the cleaner are lovers");
-        drawer.put("Newspaper",newspaper);
-        Appliance luggage = new Appliance("Luggage");
+        bed.put("bed",Bed);
+        Appliance drawer = new Appliance("drawer");
+        Artifact newspaper=new Artifact("newspaper", "patient and the cleaner are lovers");
+        drawer.put("newspaper",newspaper);
+        Appliance luggage = new Appliance("luggage");
         Artifact phone=new Artifact("phone", "contains a message with his agent: turn down all recent work.");
-        luggage.put("Phone",phone);
-        Appliance couch = new Appliance("Couch");
+        luggage.put("phone",phone);
+        Appliance couch = new Appliance("couch");
         Artifact Couch = new Artifact("couch", "null");
-        couch.put("Couch",Couch);
-        Appliance TV = new Appliance("TV");
-        Artifact tv = new Artifact("TV", "null");
-        TV.put("TV",tv);
+        couch.put("couch",Couch);
+        Appliance TV = new Appliance("tv");
+        Artifact tv = new Artifact("tv", "null");
+        TV.put("tv",tv);
         wardAppliance.put("bed",bed);
         wardAppliance.put("drawer",drawer);
         wardAppliance.put("luggage",luggage);
         wardAppliance.put("couch",couch);
-        wardAppliance.put("TV",TV);
+        wardAppliance.put("tv",TV);
         Room patientWard = new Room("Patient's Ward",wardAppliance, "Patient: bloodType = O; 28 years old; male; 177cm, 75kg; description: The dean gave him a plastic surgery that failed.");
 
 
         Hashtable<String, Appliance> deanAppliance = new Hashtable<String, Appliance>();
-        Appliance couch1 = new Appliance("Couch");
-        Appliance table = new Appliance("Table");
-        Appliance bookShelf = new Appliance("Bookshelf");
-        Appliance potPlant = new Appliance("Pot plant");
-        Appliance corpse = new Appliance("Corpse");
-        Appliance Luggage = new Appliance("Luggage");
+        Appliance couch1 = new Appliance("couch");
+        Appliance table = new Appliance("table");
+        Appliance bookShelf = new Appliance("bookshelf");
+        Appliance potPlant = new Appliance("plant");
+        Appliance corpse = new Appliance("corpse");
+        Appliance Luggage = new Appliance("luggage");
         Artifact bottle = new Artifact("burnt bottle", "medical...");
         Artifact report = new Artifact("surgery report","patient's surgery report");
         Artifact liquor = new Artifact("half bottle of liquor","");
@@ -88,86 +88,84 @@ public class Game {
         Artifact letter = new Artifact("letter", "a detective agency asks dean to pay the money for investigating all his staff.");
         Artifact pen = new Artifact("recording pen", "I'm back. Let's meet at 20:00 in my office");
         Artifact phone1 = new Artifact("phone", "There's an email with investor. Investor is mad about the surgery accident of his ex-girlfriend");
-        couch1.put("Bottle",bottle);
-        table.put("Report",report);
-        table.put("Liquor",liquor);
-        bookShelf.put("Profile",profile);
-        potPlant.put("Wedding Ring",weddingRing);
-        corpse.put("Corpse",Corpse);
-        Luggage.put("Letter",letter);
-        Luggage.put("Pen",pen);
-        Luggage.put("Phone",phone1);
+        couch1.put("bottle",bottle);
+        table.put("report",report);
+        table.put("liquor",liquor);
+        bookShelf.put("profile",profile);
+        potPlant.put("wedding ring",weddingRing);
+        corpse.put("corpse",Corpse);
+        Luggage.put("letter",letter);
+        Luggage.put("pen",pen);
+        Luggage.put("phone",phone1);
         deanAppliance.put("couch", couch1);
         deanAppliance.put("table", table);
-        deanAppliance.put("book shelf", bookShelf);
-        deanAppliance.put("pot plant", potPlant);
+        deanAppliance.put("bookshelf", bookShelf);
+        deanAppliance.put("plant", potPlant);
         deanAppliance.put("corpse", corpse);
         deanAppliance.put("luggage", Luggage);
         Room deanOffice = new Room("Dean's Office",deanAppliance, "Dean: bloodType = A; 33 years old; male; 179cm, 76kg; salary= 20000.");
 
 
         Hashtable<String, Appliance> cleanerAppliance = new Hashtable<String, Appliance>();
-        Appliance closet = new Appliance("Closet");
-        Appliance table1 = new Appliance("Table");
-        Appliance box = new Appliance("Storage Box");
-        Appliance drawer1 = new Appliance("Drawer");
-        cleanerAppliance.put("Closet", closet);
-        cleanerAppliance.put("Table", table1);
-        cleanerAppliance.put("Storage Box", box);
-        cleanerAppliance.put("Drawer", drawer1);
-        Artifact poster=new Artifact("Posters of a film", "Protagonist is a patient");
-        Artifact photos=new Artifact("Photos", "Photos of the cleaner");
-        Artifact deliveryForms=new Artifact("Delivery Forms", "Seems to be to steal and sell things from the hospital");
-        Artifact lvBag=new Artifact("Louis Vuitton Bag", "An expensive handbag");
-        Artifact creditCards=new Artifact("Credit Cards", "An assortment of credit cards");
-        Artifact facialmask=new Artifact("Facial mask", "A nurses surgical mask");
-        Artifact prosthetics=new Artifact("Prosthetic", "A prosthetic hand");
-        closet.put("Poster",poster);
-        table1.put("Photos",photos);
-        drawer1.put("Delivery Forms", deliveryForms);
-        drawer1.put("Louis Vuitton Bag", lvBag);
-        drawer1.put("Credit cards", creditCards);
-        box.put("Facial mask", facialmask);
-        box.put("Prosthetics", prosthetics);
+        Appliance closet = new Appliance("closet");
+        Appliance table1 = new Appliance("table");
+        Appliance box = new Appliance("box");
+        Appliance drawer1 = new Appliance("drawer");
+        cleanerAppliance.put("closet", closet);
+        cleanerAppliance.put("table", table1);
+        cleanerAppliance.put("box", box);
+        cleanerAppliance.put("drawer", drawer1);
+        Artifact poster=new Artifact("posters of a film", "Protagonist is a patient");
+        Artifact photos=new Artifact("photos", "Photos of the cleaner");
+        Artifact deliveryForms=new Artifact("delivery forms", "Seems to be to steal and sell things from the hospital");
+        Artifact lvBag=new Artifact("louis vuitton Bag", "An expensive handbag");
+        Artifact creditCards=new Artifact("credit Cards", "An assortment of credit cards");
+        Artifact facialmask=new Artifact("facial mask", "A nurses surgical mask");
+        Artifact prosthetics=new Artifact("prosthetic", "A prosthetic hand");
+        closet.put("poster",poster);
+        table1.put("photos",photos);
+        drawer1.put("delivery forms", deliveryForms);
+        drawer1.put("louis vuitton Bag", lvBag);
+        drawer1.put("credit cards", creditCards);
+        box.put("facial mask", facialmask);
+        box.put("prosthetics", prosthetics);
         Room cleanersDorm = new Room("Cleaner's Dorm",cleanerAppliance,"Cleaner: bloodType = O; wage = 2000; 26 years old; 170cm, 48kg; female; description: heard head nurse talking to assistant dean at 18:10; saw dean wearing a mask returned at 18:30; 19:30 found that dean's office on fire, dean was dead.");
 
 
         Hashtable<String, Appliance> astdeanAppliance = new Hashtable<String, Appliance>();
-        Appliance astdeansDrawer= new Appliance("Drawer");
-        Appliance astdeansTable= new Appliance("Table");
-        Appliance astdeansShelf = new Appliance("Shelf");
-        Appliance astdeansLaptop= new Appliance("Laptop");
-        astdeanAppliance.put("Drawer", astdeansDrawer);
-        astdeanAppliance.put("Table",  astdeansTable);
-        astdeanAppliance.put("Shelf",  astdeansShelf);
-        astdeanAppliance.put("Laptop",  astdeansShelf);
+        Appliance astdeansDrawer= new Appliance("drawer");
+        Appliance astdeansTable= new Appliance("table");
+        Appliance astdeansShelf = new Appliance("shelf");
+        Appliance astdeansLaptop= new Appliance("laptop");
+        astdeanAppliance.put("drawer", astdeansDrawer);
+        astdeanAppliance.put("table",  astdeansTable);
+        astdeanAppliance.put("shelf",  astdeansShelf);
+        astdeanAppliance.put("laptop",  astdeansShelf);
         Artifact diary1=new Artifact("diary", "(living in the shadow of dean; hate dean; a bottle of medical anesthetic");
         Artifact anasthetic=new Artifact("anasthetic", "");
         Artifact tissueBox=new Artifact("tissue box", "key to drawer inside");
         Artifact paperCrane=new Artifact("Paper crane", "The assistant dean must like the nurse");
         Artifact email1=new Artifact("email", "email with investor(assistant dean wants to replace dean; unsatisfied about the share allocation; request rejected)");
-        astdeansDrawer.put("Diary", diary1);
-        astdeansDrawer.put("Anasthetic", anasthetic);
-        astdeansTable.put("Tissue box",tissueBox);
-        astdeansShelf.put("Paper Crane", paperCrane);
-        astdeansLaptop.put("Email", email1);
+        astdeansDrawer.put("diary", diary1);
+        astdeansDrawer.put("anasthetic", anasthetic);
+        astdeansTable.put("tissue box",tissueBox);
+        astdeansShelf.put("paper crane", paperCrane);
+        astdeansLaptop.put("email", email1);
         Room astDeansOffice = new Room("Assistant Dean's Office",astdeanAppliance, "Assistant dean: bloodType = O; 31 years old; male; 177cm, 74kg; salary= 10000; description: he, dean and investor are running the hospital; his brother, the dean, has disappeared a year ago because of a surgery failure that accidentally killed a model; 18:00, dean called him and said he has returned back, asked him to meet at 20:00.");
 
 
         
         Hashtable<String, Appliance> cleaningAppliance = new Hashtable<String,Appliance>();
-        Appliance cleaningShelf= new Appliance("Shelf");
-        Appliance cleaningTrash= new Appliance("Trash bin");
-        Appliance cleaningTrolley= new Appliance("Trolley");
-        Artifact gauze=new Artifact("gauze", " ");
-        Artifact anasthetic1=new Artifact("anasthetic", " ");
-        Artifact lighter=new Artifact("lighter", " ");
-        cleaningShelf.put("Medical gauze", gauze);
-        cleaningShelf.put("Medical anasthetic", anasthetic1);
-        cleaningTrolley.put("Lighter", lighter);
-        cleaningAppliance.put("Shelf", cleaningShelf);
-        cleaningAppliance.put("Trolley",  cleaningTrolley);
-        cleaningAppliance.put("Trash bin",  cleaningTrash);
+        Appliance cleaningShelf= new Appliance("shelf");
+        Appliance cleaningTrolley= new Appliance("trolley");
+        Artifact gauze=new Artifact("gauze", "null");
+        Artifact anasthetic1=new Artifact("anasthetic", "null");
+        Artifact lighter=new Artifact("lighter", "null");
+        cleaningShelf.put("medical gauze", gauze);
+        cleaningShelf.put("medical anasthetic", anasthetic1);
+        cleaningTrolley.put("lighter", lighter);
+        cleaningAppliance.put("shelf", cleaningShelf);
+        cleaningAppliance.put("trolley",  cleaningTrolley);
         Room cleaningRoom = new Room("Cleaning Room",cleaningAppliance,"Cleaner: bloodType = O; wage = 2000; 26 years old; 170cm, 48kg; female; description: heard head nurse talking to assistant dean at 18:10; saw dean wearing a mask returned at 18:30; 19:30 found that dean's office on fire, dean was dead.");
 
 
@@ -200,7 +198,7 @@ public class Game {
                 "Welcome to the Sharlock Homles! In this game, you are a detective in a crime scene. You need to explore the space to find clues and figure out what happened. Now, what name do you want to use?");
         String nameD = input.nextLine();
         Detective detective = new Detective(nameD);
-        detective.goTo(trial.allRooms.get("patient's ward"));
+        detective.walkTo(trial.allRooms.get("patient's ward"));
         while (true) {
             System.out.println("");
             System.out.println(detective.name + ", what do you want to do next? Call 'show options' to see the availble actions");
@@ -217,39 +215,67 @@ public class Game {
                 detective.lookAround();
             } else if (response.equals("quit")) {
                 break;
-            } else if (list_words.contains("go")) {
+            } else if (list_words.contains("walk")) {
                 String loc = res_words[num_words - 2] + " " + res_words[num_words - 1];
                 // System.out.println(loc);
-                detective.goTo(trial.allRooms.get(loc));
+                try{
+                    detective.walkTo(trial.allRooms.get(loc));
+                }
+                catch(Exception e){
+                    System.out.println("cannot use walk to! see available options in show options()!");
+                }
 
-            } else if (list_words.contains("walk")) {
+            } else if (list_words.contains("go")) {
 
-                detective.walkTo(detective.location.appliances.get(res_words[num_words - 1]));
+                try{
+                    detective.goTo(detective.location.appliances.get(res_words[num_words - 1]));
+                }
+                catch(Exception e){
+                    System.out.println("cannot go to! use other options in show options()");
+                }
                 
 
             } else if (list_words.contains("examine")) {
 
-                detective.examine(detective.applianceloc.items.get(res_words[num_words - 1]));
+                try{
+                    detective.examine(detective.applianceloc.items.get(res_words[num_words - 1]));
+                }
+                catch (Exception e){
+                    System.out.println("cannot examine! use other options in show options()!");
+                }
                 
 
             } else if (list_words.contains("grab")) {
-
+                try{
                 detective.grab(detective.applianceloc.items.get(res_words[num_words - 1]));
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
                 
 
             } else if (list_words.contains("drop")) {
-
+                try{
                 detective.drop(detective.applianceloc.items.get(res_words[num_words - 1]));
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+            }
+            else if(list_words.contains("check")){
+                detective.checkBag();
+            }
                 
 
-            } else if (list_words.contains("declare")) {
+             else if (list_words.contains("declare")) {
 
                 detective.declare();
             }
-            // if(res_words[0].equals("drop")){
-            // int idx = detective.location.items.indexOf(res_words[1]);
-            // detective.drop(detective.location.items.get(idx));
-            // }
+            else if (list_words.contains("report"))
+            {
+                detective.showReport();
+            }
+            
             else {
                 System.out.println("I don't understand your command. Call 'show options' to view excutable actions. ");
             }
@@ -257,8 +283,7 @@ public class Game {
 
         input.close();
         detective.lookAround();
-        // int idx = detective.location.items.indexOf("Drawer");
-        // detective.examine(detective.location.items.get(0));
+        
 
     }
 }
